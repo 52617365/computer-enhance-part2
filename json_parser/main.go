b4b8a31cde8b1aa86de5c9790cc35fba87709841
52617365 <rasengithub@protonmail.com>
2023-09-09 12:43:40 +0300

    Json parser now takes in the JSON file path.

---
+++ json_parser/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  "io/ioutil"
+)
+
+type Arguments struct {
+  filePath string
+}
+
+func parseArguments() Arguments {
+  var filePath string
+
+  flag.StringVar(&filePath, "filePath", "", "Path to the parsable JSON file.")
+  flag.Parse()
+
+
+  if filePath == "" {
+    panic("file path is not provided")
+  }
+
+  return Arguments {
+    filePath: filePath,
+  }
+}
+
+func readFileToString(filePath string) string {
+  data, err := ioutil.ReadFile(filePath)
+
+  if err != nil {
+    panic("Error reading the JSON file contents")
+  }
+
+  dataString := string(data)
+
+  return dataString
+}
+
+func main() {
+  arguments := parseArguments()
+
+  data := readFileToString(arguments.filePath)
+
+  fmt.Printf("-------------\n")
+  fmt.Printf("File path: %s\n", arguments.filePath)
+  fmt.Printf("Data:\n%s\n", data)
+  fmt.Printf("-------------\n")
+}
+
+
+ 
+func parseJson(jsonData string) map[string]any {
+
+
+  return map[string]interface{}{"hello": "test"}
+}
